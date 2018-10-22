3f5aa084c5e597a34b33435614d37772a95c291c
houguizhao <891855179@qq.com>
2018-10-22 09:09:47 +0800

    引入minapp-api-promise

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App';
 import fly from './utils/fly';
+import WXP from 'minapp-api-promise';
 import './../static/weui.css';
 import 'font-awesome/css/font-awesome.min.css';
 Vue.config.productionTip = false;
@@ -8,5 +9,6 @@ App.mpType = 'app';
 
 const app = new Vue(App);
 app.$mount();
+Vue.prototype.$wx = WXP;
 Vue.prototype.$http = fly;
 Vue.prototype.globalData = getApp();
